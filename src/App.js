f83d070104c8072ebc6b9dfa8b74c3d83a54e7dd
Maria Ramos <mariaeramosmorales@gmail.com>
2020-12-09 10:40:04 -0400

    Show first image per item.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/scss/style.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container">
+        <div id="background">
+            <div id="body"></div>
+            <div id="eyes" className="eyes1"></div>
+            <div id="ears" className="ears1"></div>
+            <div id="nose" className="nose1"></div>
+            <div id="mouth" className="mouth1"></div>
+            <div id="clothes" className="clothes1"></div>
+        </div>
+      </div>
     </div>
   );
 }
